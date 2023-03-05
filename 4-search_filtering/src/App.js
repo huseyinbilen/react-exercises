02ac5f6099db681dffdcdd315c909e65b5692dea
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-03-05 19:37:46 +0300

    Project Cretaed SearchBar Added

---
+++ 4-search_filtering/src/App.js
@@ -0,0 +1,14 @@
+import SearchBar from "./components/SearchBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App_body">
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 4-search_filtering/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import './SearchBar.css'
+
+function SearchBar() {
+    return(
+        <div>
+            <input type='text' placeHolder='Search...' />
+        </div>
+    );
+}
+
+export default SearchBar;
