517abaff6cfb3ccf386e0211edcfd36d6b25448e
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-03-08 23:40:54 +0300

    Option Component Created

--- 6-quiz_app/src/components/Card.js
+++ 6-quiz_app/src/components/Card.js
@@ -1,9 +1,11 @@
+import Option from './Option';
 import './Card.css';
 
 function Card() {
     return (
         <div className="card_body">
-
+            <h3>Lorem Ipsum Dolor Sit Amet?</h3>
+            <Option />
         </div>
     );
 }
---
+++ 6-quiz_app/src/components/Option.js
@@ -0,0 +1,11 @@
+import './Option.css';
+
+function Option() {
+    return (
+        <div className="option_body">
+            <h4>asdad</h4>
+        </div>
+    );
+}
+
+export default Option;
