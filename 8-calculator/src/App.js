1074523669b26501aa000e401865ed1ea72d36bb
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-03-18 23:05:43 +0300

    Display Added

--- 8-calculator/src/App.js
+++ 8-calculator/src/App.js
@@ -1,10 +1,11 @@
+import Display from './components/Display';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App_body">
-        
+        <Display />
       </div>
     </div>
   );
---
+++ 8-calculator/src/components/Display.js
@@ -0,0 +1,10 @@
+import './Display.css';
+function Display() {
+    return (
+        <div>
+            <input type="text"></input>
+        </div>
+    );
+}
+
+export default Display;
