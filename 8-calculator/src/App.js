130e712fbf2792fd8694e2e2a758393c7b435bc0
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-03-24 17:44:32 +0300

    Project Completed

--- 8-calculator/src/App.js
+++ 8-calculator/src/App.js
@@ -1,21 +1,10 @@
-import { useState } from 'react';
-import Display from './components/Display';
-import ButtonGroup from './components/ButtonGroup'
-import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
-  const [input, setInput] = useState(0);
-
-  const displayChange = (val) => {
-    let temp = input;
-    setInput(`${temp}`+`${val}`)
-  }
-
   return (
     <div className="App">
       <div className="App_body">
-        <Display value={input}/>
-        <ButtonGroup addProcess={displayChange}/>
+        <Calculator />
       </div>
     </div>
   );
